50588c06d237c09103bdde8407b415b80f341f79
Paulo Henrique <paulosilvacolegioatenas@gmail.com>
2026-06-18 20:23:10 -0300

    feat: cria entidade Equipe e interface EquipeRepository

---
+++ src/main/java/com/esports/gestaotorneios/model/Equipe.java
@@ -0,0 +1,58 @@
+package com.esports.gestaotorneios.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "equipes")
+public class Equipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nome;
+
+    @Column(nullable = false, length = 5)
+    private String tag;
+
+    private LocalDate dataFundacao;
+
+    // Construtor padrão exigido pelo JPA
+    public Equipe() {
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public LocalDate getDataFundacao() {
+        return dataFundacao;
+    }
+
+    public void setDataFundacao(LocalDate dataFundacao) {
+        this.dataFundacao = dataFundacao;
+    }
+}
---
+++ src/main/java/com/esports/gestaotorneios/repository/EquipeRepository.java
@@ -0,0 +1,9 @@
+package com.esports.gestaotorneios.repository;
+
+import com.esports.gestaotorneios.model.Equipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EquipeRepository extends JpaRepository<Equipe, Long> {
+}
